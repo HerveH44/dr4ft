0ffb02d7fb5d4be4501c1739f8439bc65b93658a
HerveH44 <herve.huneau@gmail.com>
2017-09-23 00:11:26 +0200

    Add support for Heroku webapp

--- public/src/app.js
+++ public/src/app.js
@@ -93,7 +93,7 @@ let App = {
     let options = {
       query: { id, name }
     }
-    let ws = this.ws = eio(location.host, options)
+    let ws = this.ws = eio(location.href, options)
     ws.on('open' , ()=> console.log('open'))
     ws.on('close', ()=> console.log('close'))
     ws.on('message', message)
